fff45671001ac507593ce41069cbe3842e10a5d6
Abhishek Singh Thakur <31814252+Abhishek-singh9@users.noreply.github.com>
2020-08-26 18:19:50 -0400

    E-Commerce Shopping Cart

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDNhHhzi8Mbe_hXpA4H_6CYCGw0UzDTwbg",
+    authDomain: "oshop-fce16.firebaseapp.com",
+    databaseURL: "https://oshop-fce16.firebaseio.com",
+    projectId: "oshop-fce16",
+    storageBucket: "oshop-fce16.appspot.com",
+    messagingSenderId: "540383509827",
+    appId: "1:540383509827:web:00dc54cebdc12551d7c3f6",
+    measurementId: "G-WE20RWZRK5"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDNhHhzi8Mbe_hXpA4H_6CYCGw0UzDTwbg",
+    authDomain: "oshop-fce16.firebaseapp.com",
+    databaseURL: "https://oshop-fce16.firebaseio.com",
+    projectId: "oshop-fce16",
+    storageBucket: "oshop-fce16.appspot.com",
+    messagingSenderId: "540383509827",
+    appId: "1:540383509827:web:00dc54cebdc12551d7c3f6",
+    measurementId: "G-WE20RWZRK5"
+  }
 };
 
 /*
